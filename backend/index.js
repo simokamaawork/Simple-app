e7964f7f9bf2f1f995173715b7fe137bd30f5e2d
Simon Kamau <simonkamau690@gmail.com>
2023-11-29 17:05:13 +0300

    system commit

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,15 @@
-console.log("hello world")
+const express = require("express");
+const app = express()
+const port  = 3000
+
+app.get('/', (req, res)=>{
+    res.send("hello i love you and even more")
+})
+
+app.get('/', (req, res)=>{
+    res.send("")
+})
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+})
