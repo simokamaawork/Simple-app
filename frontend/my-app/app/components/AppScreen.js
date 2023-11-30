c97bb92e4a75ba8663c453ca599045f434113733
Simon Kamau <simonkamau690@gmail.com>
2023-11-30 18:15:05 +0300

    initial setup

---
+++ frontend/my-app/app/components/AppScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AppScreen = () => {
+  return (
+    <View>
+      <Text>AppScreen</Text>
+    </View>
+  )
+}
+
+export default AppScreen
+
+const styles = StyleSheet.create({})
