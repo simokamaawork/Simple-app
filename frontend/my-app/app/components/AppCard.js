66dc9a60820bd5bcc290d6f976ef153470c90820
Simon Kamau <simonkamau690@gmail.com>
2023-12-01 14:13:51 +0300

    listings

---
+++ frontend/my-app/app/components/AppCard.js
@@ -0,0 +1,17 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AppCard = () => {
+  return (
+    <View>
+      <Text>AppCard</Text>
+      <Image
+      src='{""}'
+      />
+    </View>
+  )
+}
+
+export default AppCard
+
+const styles = StyleSheet.create({})
---
+++ frontend/my-app/app/screens/listings.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const listings = () => {
+  return (
+    <View>
+      <Text>listings</Text>
+    </View>
+  )
+}
+
+export default listings
+
+const styles = StyleSheet.create({})
