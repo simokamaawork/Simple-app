b90cad0b552da5085b564b20550e0429eed4d123
Simon Kamau <simonkamau690@gmail.com>
2023-12-01 11:36:04 +0300

    card components

--- frontend/my-app/App.js
+++ frontend/my-app/App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './app/screens/LoginScreen';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <LoginScreen/>
+      
       <StatusBar style="auto" />
     </View>
   );
