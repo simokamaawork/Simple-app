097502649e5203ff8baf39982700e480f0260949
Simon Kamau <simonkamau690@gmail.com>
2023-12-01 13:30:43 +0300

    account screen

---
+++ frontend/my-app/app/screens/AccountScreen.js
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import AppScreen from '../components/AppScreen'
+import AppText from '../components/AppText'
+
+const AccountScreen = () => {
+  return (
+<AppScreen>
+<AppText>Account Screen</AppText>
+</AppScreen>
+  )
+}
+
+export default AccountScreen
+
+const styles = StyleSheet.create({})
